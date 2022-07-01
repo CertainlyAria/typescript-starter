50891f967b8f54ae278a5de954a8e0ff4abb2532
Aria <certainlyAria@gmail.com>
2022-07-01 19:01:19 +0200

    style(formatter): install prettier & setup format scripts

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import("prettier").Config}
+ */
+ module.exports = {
+    tabWidth: 4,
+    trailingComma: "all",
+    /* keep in sync with
+           rules["max-len"].code
+         in .eslintrc.json
+    */
+    printWidth: 80,
+};
