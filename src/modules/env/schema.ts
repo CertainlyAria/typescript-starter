1f0ce141170a65ee23b1eeb2c0b0795264529ee3
Aria <certainlyaria@gmail.com>
2023-11-19 11:33:18 +0100

    refactor: simplify schema of environment variables

--- src/modules/env/schema.ts
+++ src/modules/env/schema.ts
@@ -2,23 +2,7 @@ import { z } from "zod";
 
 const envSchema = z.object({
     NODE_ENV: z.enum(["development", "production", "test"]),
-    EXPRESS_PORT: z
-        .string()
-        .superRefine((val, ctx) => {
-            const castResult = z
-                .preprocess(
-                    (val) => parseFloat(val as string),
-                    z.number().int().min(0).max(65535),
-                )
-                .safeParse(val);
-
-            if (!castResult.success) {
-                for (const castIssue of castResult.error.errors) {
-                    ctx.addIssue(castIssue);
-                }
-            }
-        })
-        .transform((val) => parseInt(val)),
+    EXPRESS_PORT: z.string().pipe(z.coerce.number().int().min(0).max(65535)),
 });
 
 export { envSchema };
